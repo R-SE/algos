67fde9d6fb0b94f8e94dbe091b254d59288e0659
Rose <rose.lin.swe@gmail.com>
2021-04-18 19:17:26 -0700

    Add factorial funcs

---
+++ math/factorial.cpp
@@ -0,0 +1,20 @@
+int factorial_recursive(int n)
+{
+  if (n <= 1)
+  {
+    return 1;
+  }
+  return n * factorial_recursive(n - 1);
+}
+
+int factorial_iterative(int n)
+{
+  int count = 1;
+  int product = 1;
+  while (count <= n)
+  {
+    product *= count;
+    count++;
+  }
+  return product;
+}
