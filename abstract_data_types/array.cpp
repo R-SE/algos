a21d62076fcb0ce2485dc8f0682f989e22ca4797
Rose <rose.lin.swe@gmail.com>
2021-05-09 13:24:25 -0700

    Add new functions for Array ADT - getMin, getMax, getSum, getAverage

--- abstract_data_types/array.cpp
+++ abstract_data_types/array.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 
 class Array
 {
@@ -14,7 +15,15 @@ public:
   int insert(int num, int idx);
   int deleteAtIdx(int idx);
   int search(int num);
-  // private:
+  int get(int idx);
+  int set(int idx, int num);
+  int getMin();
+  int getMax();
+  int getSum();
+  float getAverage();
+
+private:
+  int getMinOrMax(bool isMin);
 };
 
 // Constructor for ADT Array.
@@ -41,6 +50,7 @@ void Array::display()
   {
     std::cout << a[i] << " ";
   }
+  std::cout << std::endl;
 }
 
 // Append to end of the array if there's space, and return number of elements after.
@@ -110,6 +120,82 @@ int Array::search(int num)
   return -1;
 }
 
+// Get the number at the given index.
+// Return -1 if out of bounds.
+// Runtime: O(1) Constant time array lookup
+int Array::get(int idx)
+{
+  if (idx < 0 || idx >= length)
+  {
+    return -1;
+  }
+
+  return a[idx];
+}
+
+// Set element at given index if within bounds and return new length,
+// otherwise do nothing and return -1.
+int Array::set(int idx, int num)
+{
+  if (idx < 0 || idx >= length)
+  {
+    return -1;
+  }
+
+  if (idx == length - 1)
+  {
+    return add(num);
+  }
+
+  a[idx] = num;
+  return length;
+}
+
+// Get min or max integer of array.
+int Array::getMinOrMax(bool isMin)
+{
+  int num = isMin ? std::numeric_limits<int>::max() : std::numeric_limits<int>::min();
+
+  for (int i = 0; i < length; i++)
+  {
+    if (isMin && a[i] < num)
+    {
+      num = a[i];
+    }
+    else if (!isMin && a[i] > num)
+    {
+      num = a[i];
+    }
+  }
+
+  return num;
+}
+
+int Array::getMin()
+{
+  return getMinOrMax(true);
+}
+
+int Array::getMax()
+{
+  return getMinOrMax(false);
+}
+
+int Array::getSum()
+{
+  int sum = 0;
+  for (int i = 0; i < length; i++)
+  {
+    sum += a[i];
+  }
+  return sum;
+}
+
+float Array::getAverage()
+{
+  return (float)getSum() / length;
+}
+
 int main()
 {
   Array arr(10);
@@ -118,10 +204,15 @@ int main()
   arr.add(6);
   arr.add(7);
   arr.add(8);
+  arr.add(9);
   arr.display();
   arr.insert(1, 2);
   arr.display();
   arr.deleteAtIdx(3);
   arr.display();
-  std::cout << arr.search(5);
+  std::cout << "Linear searching for num 5 " << arr.search(5) << std::endl;
+  std::cout << "Getting min " << arr.getMin() << std::endl;
+  std::cout << "Getting max " << arr.getMax() << std::endl;
+  std::cout << "Getting sum " << arr.getSum() << std::endl;
+  std::cout << "Getting average " << arr.getAverage() << std::endl;
 }
